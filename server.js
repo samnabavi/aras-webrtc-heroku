22b60e3d8ec27a952155c7e1482622fb383b85e3
samnabavi <snabavi@ucsd.edu>
2021-04-11 15:41:03 -0700

    first

--- server.js
+++ server.js
@@ -14,10 +14,10 @@ io.on('connection', socket => {
     socket.on("join room", roomID => {
         if (users[roomID]) {
             const length = users[roomID].length;
-            if (length === 4) {
-                socket.emit("room full");
-                return;
-            }
+            // if (length === 4) {
+            //     socket.emit("room full");
+            //     return;
+            // }
             users[roomID].push(socket.id);
         } else {
             users[roomID] = [socket.id];
@@ -46,7 +46,13 @@ io.on('connection', socket => {
     });
 
 });
+if (process.env.PROD) {
+    app.use(express.static(path.join(dirname, './client/build')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(dirname, './client/build/index.html'))
+    })
 
+}
 server.listen(process.env.PORT || 8000, () => console.log('server is running on port 8000'));
 
 
